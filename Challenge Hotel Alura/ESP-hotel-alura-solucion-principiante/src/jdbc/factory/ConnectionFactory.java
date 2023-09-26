35a3943961b408f8d48a196af9ccb7172b08abca
Diogenes Quintero <dio-quincar@outlook.com>
2023-09-26 07:13:13 -0500

    First Commit

---
+++ Challenge Hotel Alura/ESP-hotel-alura-solucion-principiante/src/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,32 @@
+package jdbc.factory;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel_alura?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("diogenes");
+		
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexion() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
